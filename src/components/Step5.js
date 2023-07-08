ff12d2c39f860b4605f7d7b859ff81d92144fb8e
Nafiz-Anam <121781159+Nafiz-PureTech@users.noreply.github.com>
2023-07-08 20:19:20 +0600

    step5 screen created

--- src/components/Step5.js
+++ src/components/Step5.js
@@ -1,257 +1,84 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
-import {
-  // makeStyles,
-  FormControl,
-  RadioGroup,
-  FormControlLabel,
-  Radio,
-  Card,
-  CardContent,
-  CircularProgress,
-  Typography,
-} from "@material-ui/core";
-
-// const useStyles = makeStyles((theme) => ({
-//   form: {
-//     display: "flex",
-//     flexDirection: "column",
-//     alignItems: "center",
-//   },
-//   textField: {
-//     margin: theme.spacing(2),
-//   },
-//   submitButton: {
-//     marginTop: theme.spacing(2),
-//   },
-// }));
-
-const sheetURL =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vQORHI8-xEc9MatJrHUWA-hUyuLVl6tmfkLLOVGoB7WmZwD6e98ZKK04ebEZkcKOdZI1uPWj0otsUNt/pub?gid=1055355179&single=true&output=csv";
 
 function Step() {
-  const [sheetData, setSheetData] = useState([]);
-  const [selectedOption, setSelectedOption] = useState("");
-  const [pickupTimeSlotPrice, setPickupTimeSlotPrice] = useState(0);
+    const sheetURL =
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vQORHI8-xEc9MatJrHUWA-hUyuLVl6tmfkLLOVGoB7WmZwD6e98ZKK04ebEZkcKOdZI1uPWj0otsUNt/pub?gid=1055355179&single=true&output=csv";
 
-  useEffect(() => {
-    setSelectedOption(
-      localStorage.getItem("selectedPickupTimeSlot") != null
-        ? JSON.parse(localStorage.getItem("selectedPickupTimeSlot"))
-        : ""
-    );
-  }, []);
+    const [sheetData, setSheetData] = useState([]);
+    const [selectedItem, setSelectedItem] = useState("");
 
-  useEffect(() => {
-    try {
-      var price = pickupTimeSlotPrice.replace("€", "");
-      localStorage.setItem("pickupTimeSlotPrice", parseInt(price));
-    } catch (error) {
-      // alert(error);
-    }
-  }, [pickupTimeSlotPrice]);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(sheetURL);
+                const csvData = response.data;
+                // Parse the CSV data
+                const parsedData = parseCSV(csvData);
+                setSheetData(parsedData);
+                // globalSheetData = parsedData;
+            } catch (error) {
+                console.error("Error retrieving CSV data:", error);
+            }
+        };
+        fetchData();
+    }, []);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(sheetURL);
-        const parsedData = parseCSV(response.data);
-        setSheetData(parsedData);
-      } catch (error) {
-        console.error("Error retrieving CSV data:", error);
-      }
+    // Function to parse CSV data into an array of rows
+    const parseCSV = (csvData) => {
+        const rows = csvData.split("\n");
+        const parsedData = [];
+        for (let i = 0; i < rows.length; i++) {
+            const row = rows[i].split(",");
+            //if i > 30, break
+            if (i > 30) {
+                break;
+            }
+            parsedData.push(row);
+        }
+        //sheetData is an array of items
+        return parsedData;
     };
 
-    fetchData();
-  }, []);
+    console.log("sheetData", sheetData);
 
-  const parseCSV = (csvData) => {
-    const rows = csvData.split("\n");
-    const parsedData = rows.map((row) => row.split(","));
-    return parsedData;
-  };
+    let jsonArray = [];
+    sheetData.forEach((data) => {
+        // Create an object with the required properties
+        let obj = {
+            time: data[0],
+            price: data[1],
+        };
+        // Push the object to the array
+        jsonArray.push(obj);
+    });
 
-  const handleSelectChange = (event) => {
-    const value = event.target.value;
-    setSelectedOption(value);
-    localStorage.setItem("selectedPickupTimeSlot", JSON.stringify(value));
-    var element = sheetData.find((element) => element[0] === value);
-    setPickupTimeSlotPrice(element[1]);
-    //alert(element[1]);
-  };
-  const isPhone = window.innerWidth < 600;
-  return (
-    <div>
-      <Typography
-        variant="h5"
-        style={{
-          marginBottom: "20px",
-          marginTop: "20px",
-          fontWeight: "bold",
-          //center it all
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          fontFamily: "Poppins",
-        }}
-      >
-        Select Pickup Time Slot
-      </Typography>
+    console.log("jsonArray", jsonArray);
 
-      <div
-        style={{
-          overflowY: "scroll",
-          maxWidth: "100%",
-          //full width
-          width: "100%",
-        }}
-      >
-        {" "}
-        <div
-          style={{
-            width: "100%",
-            height: "60vh",
-            //have eerything centered
-            display: "flex",
-            flexDirection: "column",
+    const handleItemClick = (itemValue) => {
+        setSelectedItem(itemValue);
+    };
 
-            //center it all
-            margin: "auto",
-            fontFamily: "Poppins",
-          }}
-        >
-          {//if shhet data length is 0, show a loading spinner
-          sheetData.length === 0 ? (
-            <CircularProgress
-              style={{
-                width: "100px",
-                height: "100px",
-                //center it all
-                margin: "auto",
-              }}
-            />
-          ) : (
-            <div></div>
-          )}
-          <FormControl
-            style={{
-              width: "100%",
-              //position it starting from the top
-              marginTop: "10px",
-              //center it all
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <RadioGroup
-              aria-label="pickupDate"
-              name="pickupDate"
-              value={selectedOption}
-              onChange={handleSelectChange}
-              style={{
-                width: "100%",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              {sheetData.map((item) => {
-                const isSelected = item[0] === selectedOption;
-                return (
-                  <FormControlLabel
-                    value={item[0]}
-                    control={
-                      <Radio
-                        style={{
-                          display: "none",
-                        }}
-                      />
-                    }
-                    label={
-                      <Card
-                        style={{
-                          // width: "100%",
-                          display: "flex",
-                          flexDirection: "column",
-                          alignItems: "center",
-                          justifyContent: "center",
-                          marginBottom: "30px",
-                          borderRadius: "10px",
-                          //if the item is selected, change the background color
-                          backgroundColor: isSelected ? "#F5F5F5" : "white",
-                          width: isPhone ? "100%" : "500px",
-                        }}
-                      >
-                        <CardContent
-                          style={{
-                            width: "100%",
-                            display: "flex",
-                            flexDirection: "row",
-                            alignItems: "center",
-                            justifyContent: "space-between",
-                          }}
+    return (
+        <div className="list-layout">
+            <ul className="list">
+                {jsonArray.map((item) => (
+                    <li key={item.time}>
+                        <button
+                            className={`list-item2 ${
+                                selectedItem === item.time ? "selected" : ""
+                            }`}
+                            onClick={() => handleItemClick(item.time)}
                         >
-                          <Typography
-                            variant="h6"
-                            style={{
-                              marginBottom: "10px",
-                              marginTop: "10px",
-                              fontWeight: "bold",
-                              marginLeft: "10px",
-                              marginRight: "10px",
-                              fontFamily: "Poppins",
-                            }}
-                          >
-                            {item[0]}{" "}
-                            {/* Moved the item label inside the Card */}
-                          </Typography>
-
-                          <Typography
-                            variant="h6"
-                            style={{
-                              marginBottom: "10px",
-                              marginTop: "10px",
-                              fontWeight: "bold",
-                              marginLeft: "10px",
-                              marginRight: "10px",
-                              fontFamily: "Poppins",
-                            }}
-                          >
-                            {item[1]}
-                          </Typography>
-
-                          {/* <Typography
-                            variant="h3"
-                            style={{
-                              marginBottom: "10px",
-                              marginTop: "10px",
-                              fontWeight: "bold",
-                              color: "#6A1B9A",
-                              marginLeft: "10px",
-
-                              fontFamily: "Poppins",
-                              //self align it to the right
-                              alignSelf: "flex-end",
-                            }}
-                          >
-                            {item[2]}
-                          </Typography> */}
-                        </CardContent>
-                      </Card>
-                    }
-                  />
-                );
-              })}
-            </RadioGroup>
-          </FormControl>
+                            <span className="item-name">{item.time}</span>
+                            <span className="item-name">{item.price}</span>
+                        </button>
+                    </li>
+                ))}
+            </ul>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Step;
