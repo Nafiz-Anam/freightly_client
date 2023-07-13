3e885c214447c9e2b37ecd93887f6d0fe983a77c
Nafiz Anam <nafiz.anam002@gmail.com>
2023-07-14 03:32:56 +0600

    form ui done

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-// import { DropzoneArea } from "material-ui-dropzone";
 import style from "./SearchComponent.module.css";
 
-const SearchComponent = ({ items }) => {
+const SearchComponent = ({ items, setModalShow, setSelectedItem }) => {
     const [searchTerm, setSearchTerm] = useState("");
     const [searchResults, setSearchResults] = useState([]);
-    const [selectedItems, setSelectedItems] = useState([]);
+
+    // let items = [["bike"], ["car"], ["bed"]];
 
     let products = [];
     for (let val of items) {
@@ -28,67 +28,8 @@ const SearchComponent = ({ items }) => {
     };
 
     const handleItemClick = (item) => {
-        const newItem = {
-            name: item.title,
-            width: "",
-            length: "",
-            height: "",
-            images: [],
-            count: "",
-        };
-        setSelectedItems((prevItems) => [...prevItems, newItem]);
-    };
-
-    const handleWidthChange = (e, index) => {
-        const { value } = e.target;
-        setSelectedItems((prevItems) => {
-            const updatedItems = [...prevItems];
-            updatedItems[index].width = value;
-            return updatedItems;
-        });
-    };
-
-    const handleLengthChange = (e, index) => {
-        const { value } = e.target;
-        setSelectedItems((prevItems) => {
-            const updatedItems = [...prevItems];
-            updatedItems[index].length = value;
-            return updatedItems;
-        });
-    };
-
-    const handleHeightChange = (e, index) => {
-        const { value } = e.target;
-        setSelectedItems((prevItems) => {
-            const updatedItems = [...prevItems];
-            updatedItems[index].height = value;
-            return updatedItems;
-        });
-    };
-
-    const handleImageDrop = (images, index) => {
-        setSelectedItems((prevItems) => {
-            const updatedItems = [...prevItems];
-            updatedItems[index].images = images;
-            return updatedItems;
-        });
-    };
-
-    const handleCountChange = (e, index) => {
-        const { value } = e.target;
-        setSelectedItems((prevItems) => {
-            const updatedItems = [...prevItems];
-            updatedItems[index].count = value;
-            return updatedItems;
-        });
-    };
-
-    const handleAddButtonClick = () => {
-        // Save selected items with input values
-        console.log(selectedItems);
-
-        // Clear input fields
-        setSelectedItems([]);
+        setSelectedItem(item.title);
+        setModalShow(false);
     };
 
     return (
@@ -112,72 +53,6 @@ const SearchComponent = ({ items }) => {
                     </li>
                 ))}
             </ul>
-
-            {selectedItems.length > 0 && (
-                <div>
-                    <h2>Selected Items</h2>
-                    {selectedItems.map((item, index) => (
-                        <div key={index}>
-                            <h3>{item.name}</h3>
-                            <div>
-                                <label>Width:</label>
-                                <input
-                                    type="text"
-                                    value={item.width}
-                                    onChange={(e) =>
-                                        handleWidthChange(e, index)
-                                    }
-                                />
-                            </div>
-                            <div>
-                                <label>Length:</label>
-                                <input
-                                    type="text"
-                                    value={item.length}
-                                    onChange={(e) =>
-                                        handleLengthChange(e, index)
-                                    }
-                                />
-                            </div>
-                            <div>
-                                <label>Height:</label>
-                                <input
-                                    type="text"
-                                    value={item.height}
-                                    onChange={(e) =>
-                                        handleHeightChange(e, index)
-                                    }
-                                />
-                            </div>
-                            <div>
-                                <label>Images:</label>
-                                {/* <DropzoneArea
-                                    acceptedFiles={["image/*"]}
-                                    filesLimit={3}
-                                    dropzoneText="Drag and drop or click to upload images"
-                                    onChange={(files) =>
-                                        handleImageDrop(files, index)
-                                    }
-                                /> */}
-                            </div>
-                            <div>
-                                <label>Count:</label>
-                                <input
-                                    type="text"
-                                    value={item.count}
-                                    onChange={(e) =>
-                                        handleCountChange(e, index)
-                                    }
-                                />
-                            </div>
-                        </div>
-                    ))}
-                    <button onClick={handleAddButtonClick}>Add</button>
-                </div>
-            )}
-
-            {/* Implement the new view to display the selected items */}
-            {/* You can use the state variable 'selectedItems' to render the list of items */}
         </div>
     );
 };
