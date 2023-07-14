19059f16e4a3663b50e95957769e160fd26d0088
Nafiz Anam <nafiz.anam002@gmail.com>
2023-07-14 20:47:35 +0600

    modified forms

--- src/components/Step10.js
+++ src/components/Step10.js
@@ -1,131 +1,55 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { TextField } from "@material-ui/core";
-
-import Typography from "@material-ui/core/Typography";
-//import date and time picker from mui
-
-const useStyles = makeStyles((theme) => ({
-    form: {
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-    },
-    textField: {
-        margin: theme.spacing(2),
-    },
-    submitButton: {
-        marginTop: theme.spacing(2),
-    },
-}));
+import { useForm } from "react-hook-form";
+import style from "./Step11.module.css";
 
 function Step10() {
-    //if drop data does not exist in local storage just make it  having all keys but empty values
-    //if it exists then do nothing
-
-    const classes = useStyles();
-
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [phone, setPhone] = useState("");
-    //pickup address
-    const [pickupAddress, setPickupAddress] = useState("");
-    useEffect(() => {
-        // set all the states from local storage at the start if they are not empty
-        //alert(JSON.stringify(localStorage.getItem("pickupData")));
-        if (JSON.parse(localStorage.getItem("pickupData"))) {
-            if (JSON.parse(localStorage.getItem("pickupData")).name !== "") {
-                setName(JSON.parse(localStorage.getItem("pickupData")).name);
-            }
-            if (JSON.parse(localStorage.getItem("pickupData")).email !== "") {
-                setEmail(JSON.parse(localStorage.getItem("pickupData")).email);
-            }
-            if (JSON.parse(localStorage.getItem("pickupData")).phone !== "") {
-                setPhone(JSON.parse(localStorage.getItem("pickupData")).phone);
-            }
-            if (JSON.parse(localStorage.getItem("pickupData")).address !== "") {
-                setPickupAddress(
-                    JSON.parse(localStorage.getItem("pickupData")).address
-                );
-            }
-        }
-    }, []);
-    useEffect(() => {
-        //set local storage on every change
-        localStorage.setItem(
-            "pickupData",
-            JSON.stringify({
-                name: name === "" ? "" : name,
-                email: email === "" ? "" : email,
-                phone: phone === "" ? "" : phone,
-                address: pickupAddress === "" ? "" : pickupAddress,
-            })
-        );
-    }, [name, email, phone, pickupAddress]);
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+    const onSubmit = (data) => console.log(data);
+    console.log(errors);
 
     return (
-        <div style={{ display: "flex", justifyContent: "center" }}>
+        <div style={{ marginTop: "40px" }}>
+            <h2 style={{ textAlign: "center" }}>
+                Pickup address contact details
+            </h2>
             <form
-                style={{
-                    // fontFamily: "Poppins",
-                    height: "55vh",
-                    width: "70%",
-                }}
-                className={classes.form}
+                className={style.formContainer}
+                onSubmit={handleSubmit(onSubmit)}
             >
-                <Typography
-                    variant="h5"
-                    style={{
-                        marginBottom: "20px",
-                        marginTop: "20px",
-                        fontWeight: "bold",
-                        // fontFamily: "Poppins",
-                    }}
-                >
-                    Pickup Contact Information
-                </Typography>
-                <TextField
-                    label="Detailed Pickup Address"
-                    variant="outlined"
-                    className={classes.textField}
-                    value={pickupAddress}
-                    onChange={(e) => setPickupAddress(e.target.value)}
-                    fullWidth
-                    style={{
-                        fontFamily: "Poppins",
-                    }}
-                />
-                <TextField
-                    label="Name"
-                    onChange={(e) => setName(e.target.value)}
-                    fullWidth
-                    margin="normal"
-                    value={name}
-                    style={{
-                        fontFamily: "Poppins",
-                    }}
-                />
-                <TextField
-                    label="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    fullWidth
-                    margin="normal"
-                    style={{
-                        fontFamily: "Poppins",
-                    }}
-                />
-                <TextField
-                    label="Phone"
-                    value={phone}
-                    onChange={(e) => setPhone(e.target.value)}
-                    fullWidth
-                    margin="normal"
-                    style={{
-                        fontFamily: "Poppins",
-                    }}
-                />
+                <div className={style.row}>
+                    <textarea
+                        placeholder="Details address"
+                        {...register("address")}
+                    />
+                </div>
+                <div className={style.row}>
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        {...register("name", {})}
+                    />
+                </div>
+                <div className={style.row}>
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        {...register("email", {})}
+                    />
+                </div>
+                <div className={style.row}>
+                    <input
+                        type="text"
+                        placeholder="Phone"
+                        {...register("phone", {})}
+                    />
+                </div>
+                <div className={style.row}>
+                    <button type="submit"> Submit</button>
+                </div>
             </form>
         </div>
     );
--- src/components/Step11.js
+++ src/components/Step11.js
@@ -1,131 +1,53 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { TextField } from "@material-ui/core";
-
-import Typography from "@material-ui/core/Typography";
-//import date and time picker from mui
-
-const useStyles = makeStyles((theme) => ({
-    form: {
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-    },
-    textField: {
-        margin: theme.spacing(2),
-    },
-    submitButton: {
-        marginTop: theme.spacing(2),
-    },
-}));
+import { useForm } from "react-hook-form";
+import style from "./Step11.module.css";
 
 function Step11() {
-    //if drop data does not exist in local storage just make it  having all keys but empty values
-    //if it exists then do nothing
-
-    const classes = useStyles();
-
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [phone, setPhone] = useState("");
-    //drop address
-    const [dropAddress, setDropAddress] = useState("");
-    useEffect(() => {
-        // set all the states from local storage at the start if they are not empty
-        //alert(JSON.stringify(localStorage.getItem("dropData")));
-        if (JSON.parse(localStorage.getItem("dropData"))) {
-            if (JSON.parse(localStorage.getItem("dropData")).name !== "") {
-                setName(JSON.parse(localStorage.getItem("dropData")).name);
-            }
-            if (JSON.parse(localStorage.getItem("dropData")).email !== "") {
-                setEmail(JSON.parse(localStorage.getItem("dropData")).email);
-            }
-            if (JSON.parse(localStorage.getItem("dropData")).phone !== "") {
-                setPhone(JSON.parse(localStorage.getItem("dropData")).phone);
-            }
-            if (JSON.parse(localStorage.getItem("dropData")).address !== "") {
-                setDropAddress(
-                    JSON.parse(localStorage.getItem("dropData")).address
-                );
-            }
-        }
-    }, []);
-    useEffect(() => {
-        //set local storage on every change
-        localStorage.setItem(
-            "dropData",
-            JSON.stringify({
-                name: name === "" ? "" : name,
-                email: email === "" ? "" : email,
-                phone: phone === "" ? "" : phone,
-                address: dropAddress === "" ? "" : dropAddress,
-            })
-        );
-    }, [name, email, phone, dropAddress]);
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+    const onSubmit = (data) => console.log(data);
+    console.log(errors);
 
     return (
-        <div style={{ display: "flex", justifyContent: "center" }}>
+        <div style={{ marginTop: "40px" }}>
+            <h2 style={{ textAlign: "center" }}>Delivery contact details</h2>
             <form
-                style={{
-                    // fontFamily: "Poppins",
-                    height: "55vh",
-                    width: "70%",
-                }}
-                className={classes.form}
+                className={style.formContainer}
+                onSubmit={handleSubmit(onSubmit)}
             >
-                <Typography
-                    variant="h5"
-                    style={{
-                        marginBottom: "20px",
-                        marginTop: "20px",
-                        fontWeight: "bold",
-                        // fontFamily: "Poppins",
-                    }}
-                >
-                    Drop Contact Information
-                </Typography>
-                <TextField
-                    label="Detailed Drop Address"
-                    variant="outlined"
-                    className={classes.textField}
-                    value={dropAddress}
-                    onChange={(e) => setDropAddress(e.target.value)}
-                    fullWidth
-                    style={{
-                        fontFamily: "Poppins",
-                    }}
-                />
-                <TextField
-                    label="Name"
-                    onChange={(e) => setName(e.target.value)}
-                    fullWidth
-                    margin="normal"
-                    value={name}
-                    style={{
-                        fontFamily: "Poppins",
-                    }}
-                />
-                <TextField
-                    label="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    fullWidth
-                    margin="normal"
-                    style={{
-                        fontFamily: "Poppins",
-                    }}
-                />
-                <TextField
-                    label="Phone"
-                    value={phone}
-                    onChange={(e) => setPhone(e.target.value)}
-                    fullWidth
-                    margin="normal"
-                    style={{
-                        fontFamily: "Poppins",
-                    }}
-                />
+                <div className={style.row}>
+                    <textarea
+                        placeholder="Details address"
+                        {...register("address")}
+                    />
+                </div>
+                <div className={style.row}>
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        {...register("name", {})}
+                    />
+                </div>
+                <div className={style.row}>
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        {...register("email", {})}
+                    />
+                </div>
+                <div className={style.row}>
+                    <input
+                        type="text"
+                        placeholder="Phone"
+                        {...register("phone", {})}
+                    />
+                </div>
+                <div className={style.row}>
+                    <button type="submit"> Submit</button>
+                </div>
             </form>
         </div>
     );
